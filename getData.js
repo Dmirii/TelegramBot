6097baccf05325e962aa255b436147113065625d
Dima A <dimaa@dimaa.ru>
2020-10-09 09:14:58 +0300

    separate code

--- getData.js
+++ getData.js
@@ -1,9 +1,13 @@
-const getData = async (url) => {
+// функция для API fetch
+// у ноды своя fetch 
+// подключаем ее:
+const fetch = require('node-fetch');
+// експорт функции
+module.exports = async (url) => {
     const response = await fetch(url).catch( err => console.log(err));
     if(!response.ok){
         throw new Error(`Ошибка по  ${url} , статус ${respose}`)
     }
     return  await response.json();
-};
+}
 
-module.export = getData;
--- index.js
+++ index.js
@@ -1,27 +1,11 @@
-
-const keyboard = require('./keyboard.js');
-const fetch = require('node-fetch');
+const getData = require('./getData.js'); // обслуживаем API fetch
+// сам бот::
+const keyboard = require('./keyboard.js'); // описываем меню бота
 const TelegramBot = require('node-telegram-bot-api'); // подключаем node-telegram-bot-api
-
 const token = '1262373438:AAFYVbIUup6OYkFDBH1Rh4Jz6NZUdFGfsgs'; // тут токен кторый мы получили от botFather
+const bot = new TelegramBot(token, {polling: true});// создаем бота
 
-// включаем самого обота
-const bot = new TelegramBot(token, {polling: true});
-
-//
-const getData = async (url) => {
-    const response = await fetch(url).catch( err => console.log(err));
-   // console.log(response)
-    if(!response.ok){
-        throw new Error(`Ошибка по  ${url} , статус ${response}`)
-        //console.log(response)
-    }
-    return  await response.json();
-};
-
-
-
-
+// ** //
 // обработчик события присылания нам любого сообщения
 bot.on('message', (msg) => {
   const chatId = msg.chat.id; //получаем идентификатор диалога, чтобы отвечать именно тому пользователю, который нам что-то прислал
@@ -36,21 +20,18 @@ bot.on('message', (msg) => {
     }else{
         bot.sendMessage(chatId,'Я не понял')
     }
-
 });
 
-
+// ** //
 // обработчик событий нажатий на клавиатуру
 bot.on('callback_query', (query) => {
     const chatId = query.message.chat.id;
 
     let img = '';
-
-    if (query.data === 'fallOutBoy') { // если бой
-        img = 'boy.jpg';
-        
+    if (query.data === 'fallOutBoy') { // кариинка
+        img = 'boy.jpg';        
     }
-    if (query.data === 'RUB') { // если бой
+    if (query.data === 'RUB') { // курсы вылют
         
         getData('https://api.exchangeratesapi.io/latest?base=USD').then( data  => {           
             bot.sendMessage(chatId, ` USD = ${data.rates.RUB.toFixed(2)}`);           
@@ -60,24 +41,15 @@ bot.on('callback_query', (query) => {
         });        
     }
 
-    if (query.data === 'wether') { // если бой
-
-        
+    if (query.data === 'wether') { // погода        
         getData('https://api.openweathermap.org/data/2.5/weather?id=562450&appid=658790e853befcc579c25cc91de06623&units=metric')
         .then( data  => {           
-        bot.sendMessage(chatId, ` ${data.name} : мин: ${data.main.temp_min}, макс: ${data.main.temp_max}`);  
-        console.log(data) 
-        
+        bot.sendMessage(chatId, ` ${data.name} : мин: ${data.main.temp_min}, макс: ${data.main.temp_max}`);       
         });
-
-    }
-
-
-    
+    }    
 
     if (query.data === 'music') { // если музыка
-        bot.sendAudio(chatId,'./audio/1.mp3')
-        
+        bot.sendAudio(chatId,'./audio/1.mp3')        
     }
 
     if (img) {
